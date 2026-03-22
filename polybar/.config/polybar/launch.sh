77d0358642434a9d9f6bef0f740e7c0340059203
Patricio Pérez Pinto <patodeath@gmail.com>
2026-03-22 19:22:09 -0300

    Initial commit: Migration of i3, conky, redshift, polybar, dunst, fusuma, and picom configs

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminar todas las instancias existentes de Polybar
+killall -q polybar
+
+# Esperar a que los procesos se detengan
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Lanzar las barras
+polybar bar1 &
+polybar bar2 &
